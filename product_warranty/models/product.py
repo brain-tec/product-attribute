6c6eedf7c6f10afff3e00c9c7ddc7798f40473cf
BT-mgomez <miguel.gomez@braintec-group.com>
2018-07-05 12:25:37 +0200

    [t9740] Add warranty time for products.

---
+++ product_warranty/models/product.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    warranty_time = fields.Integer(string='Warranty Time',
+                                   help='Number of months before the warranty '
+                                        'of the product expires.')
