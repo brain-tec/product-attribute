e26a87834bed9ec0f1f042370d00ef2103f1bad1
Kevin Khao <kevinkhao@gmail.com>
2020-03-06 16:42:23 +0100

    [12.0][ADD] product_stock_state: reformat for OCA, misc. improvements and cleanup

--- product_stock_state/models/product_template.py
+++ product_stock_state/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-Today GRAP (http://www.grap.coop).
 # Copyright 2018 ACSONE SA/NV
 # Copyright 2018 Akretion (http://www.akretion.com).
@@ -8,6 +7,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
@@ -21,11 +21,13 @@ class ProductTemplate(models.Model):
         " use the value defined in the product category. If"
         " no value is defined in product category, it will use the value"
         " defined for the company",
+        digits=dp.get_precision("Stock Threshold"),
     )
 
-    manual_stock_state_threshold = fields.Float()
+    manual_stock_state_threshold = fields.Float(
+        digits=dp.get_precision("Stock Threshold")
+    )
 
-    @api.multi
     @api.depends(
         "categ_id.stock_state_threshold", "manual_stock_state_threshold"
     )
